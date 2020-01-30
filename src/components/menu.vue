e2e52b17dd9ee969ffd22b5e506b50ce11a3dae2
kingsleyljc <695801027@qq.com>
2020-01-30 23:05:17 +0800

    2020 1 30

---
+++ src/components/menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container>
+
+    <el-input v-model="input1" placeholder="请输入内容"></el-input>
+    <el-input v-model="input2" placeholder="请输入内容"></el-input>
+    <el-button type="primary" v-on:click="hello">主要按钮</el-button>
+
+  </el-container>
+
+</template>
+
+<script>
+export default {
+  name: 'amenu',
+  data () {
+    return {
+      input1: 120,
+      input2: 30
+    }
+  },
+  methods: {
+    hello: function () {
+      this.$emit('added', this.input1, this.input2)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
